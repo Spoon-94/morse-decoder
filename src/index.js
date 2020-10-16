451e0017b19b4de102402340065bdad486194f78
Dima Paymushkin <Dimon-94@yandex.ru>
2020-10-16 07:14:57 +0400

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let strToArray = expr.match(/(.{10})/g);
+   let arrayToMorse = strToArray.map((value) => value = value.replace(/(10)/g, `.`).replace(/(11)/g, `-`).replace(/(0)/g, ``).replace(/\*{10}/g, ` `));
+   for (let i = 0; i < arrayToMorse.length; i++) {
+      if(arrayToMorse[i] !== ` `) {
+         arrayToMorse[i] =  MORSE_TABLE[arrayToMorse[i]];
+      }
+   }
+   return arrayToMorse.join(``);
 }
 
 module.exports = {
